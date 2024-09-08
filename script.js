2d0b51cb1a146d00b4853b63ecb2a042fc919f51
amritansh-mishra <mamritansh1911@gmail.com>
2024-09-08 21:30:13 +0530

    modified:   script.js

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
 const questions= [
     {
-        questions: "Which is the largest animal in the world ?,
+        question: "Which is the largest animal in the world ?" ,
         answers:[
 
-            {  text:"shark", correct:false}
-            {  text:"Blue Whale", correct:true}
-            {  text:"Dog", correct:false}
-            {  text:"Sea Horse", correct:false}
+            {  text:"shark", correct:false},
+            {  text:"Blue Whale", correct:true},
+            {  text:"Dog", correct:false},
+            {  text:"Sea Horse", correct:false},
+        ]
+
+    },
+    {   question: "Which is the smallest animal in the world ?" ,
+        answers:[
+
+            {  text:"shark", correct:false},
+            {  text:"Blue Whale", correct:false},
+            {  text:"Dog", correct:false},
+            {  text:"Sea Horse", correct:true},
+        ]
+        
+    },
+    {  question: "Which is called king of fruits ?" ,
+        answers:[
+
+            {  text:"Mango", correct:true},
+            {  text:"Banana", correct:false},
+            {  text:"Dragon Fruit", correct:false},
+            {  text:"Litchi", correct:false},
+        ]
+
+    },
+    {
+        question: "Which is called king of jungle ?" ,
+        answers:[
+
+            {  text:"zebra", correct:false},
+            {  text:"snake", correct:false},
+            {  text:"Giraffe", correct:false},
+            {  text:"lion", correct:true},
         ]
 
     }
-]
+];
+
+const question  = document.getElementById("question");
+const answerbutton = document.getElementById("answerbuttons");
+const nxtbutton = document.getElementById("nxtbtn");
