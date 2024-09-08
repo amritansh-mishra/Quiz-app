6877746811999cde0bf29cdfa4083700f99403b7
amritansh-mishra <mamritansh1911@gmail.com>
2024-09-08 21:17:30 +0530

    modified:   inbex.html 	modified:   script.js 	modified:   style.css

---
+++ script.js
@@ -0,0 +1,13 @@
+const questions= [
+    {
+        questions: "Which is the largest animal in the world ?,
+        answers:[
+
+            {  text:"shark", correct:false}
+            {  text:"Blue Whale", correct:true}
+            {  text:"Dog", correct:false}
+            {  text:"Sea Horse", correct:false}
+        ]
+
+    }
+]
